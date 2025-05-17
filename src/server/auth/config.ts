355e233bd46b77acb41b034e756e057e3735e4d7
reddiedev <105657266+reddiedev@users.noreply.github.com>
2025-05-18 07:43:17 +0800

    Add redirect callback to auth configuration for improved URL handling
    - Implemented a redirect callback in the auth configuration to allow relative callback URLs and ensure proper handling of URLs on the same origin.
    - This enhancement improves user navigation and session management during authentication processes.

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -41,6 +41,13 @@ export const authConfig = {
 	],
 	adapter: PrismaAdapter(db),
 	callbacks: {
+		async redirect({ url, baseUrl }) {
+			// Allows relative callback URLs
+			if (url.startsWith("/")) return `${baseUrl}${url}`;
+			// Allows callback URLs on the same origin
+			else if (new URL(url).origin === baseUrl) return url;
+			return baseUrl;
+		},
 		session: ({ session, user }) => {
 			return {
 				...session,
