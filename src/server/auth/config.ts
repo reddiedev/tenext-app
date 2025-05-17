752aa7cf84b2dc016398753fddec4af19af1d6e2
reddiedev <105657266+reddiedev@users.noreply.github.com>
2025-05-17 17:39:08 +0800

    first commit

---
+++ src/server/auth/config.ts
@@ -0,0 +1,70 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import type { DefaultSession, NextAuthConfig } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { db } from "~/server/db";
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+	interface Session extends DefaultSession {
+		user: {
+			id: string;
+			// ...other properties
+			// role: UserRole;
+		} & DefaultSession["user"];
+	}
+
+	// interface User {
+	//   // ...other properties
+	//   // role: UserRole;
+	// }
+}
+
+/**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
+export const authConfig = {
+	providers: [
+		CredentialsProvider({
+			// The name to display on the sign in form (e.g. "Sign in with...")
+			name: "Credentials",
+			// `credentials` is used to generate a form on the sign in page.
+			// You can specify which fields should be submitted, by adding keys to the `credentials` object.
+			// e.g. domain, username, password, 2FA token, etc.
+			// You can pass any HTML attribute to the <input> tag through the object.
+			credentials: {
+				username: { label: "Username", type: "text", placeholder: "jsmith" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials, req) {
+				// Add logic here to look up the user from the credentials supplied
+				const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+				if (user) {
+					// Any object returned will be saved in `user` property of the JWT
+					return user;
+				}
+
+				// If you return null then an error will be displayed advising the user to check their details.
+				return null;
+			},
+		}),
+	],
+	adapter: PrismaAdapter(db),
+	callbacks: {
+		session: ({ session, user }) => ({
+			...session,
+			user: {
+				...session.user,
+				id: user.id,
+			},
+		}),
+	},
+} satisfies NextAuthConfig;
